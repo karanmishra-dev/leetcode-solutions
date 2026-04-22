cf47f220387298a8399b90364a7e3b43c4f2a293
Karan Mishra <150538561+karanmishra-dev@users.noreply.github.com>
2026-04-22 21:26:32 +0530

    Time: 0 ms (100%), Space: 11.3 MB (19.95%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -1,48 +1,17 @@
 class Solution {
-public:
-    void reverse(ListNode*head,int size){
-        ListNode*prev=NULL;
-        ListNode*curr=head;
-        ListNode*Next=NULL;
-        while(size--){
-            Next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=Next;
-        }
-        return;
-    }
+public: 
     ListNode* swapPairs(ListNode* head) {
-        if(head==NULL || head->next==NULL) return head;
-        int size=2;
-        ListNode*left=head;
-        ListNode*right;
-        ListNode*prevLeft=NULL;
-        ListNode*res=NULL;
-        while(true){
-            right=left;
-            for(int i=0;i<(size-1);i++){
-                if(right==NULL) break;
-                right=right->next;
-            }
-            if(right!=NULL){
-                ListNode*nextLeft=right->next;
-                reverse(left,size);
-                if(prevLeft!=NULL){
-                    prevLeft->next=right;
-                }
-                prevLeft=left;
-                if(res==NULL) res=right;
-                left=nextLeft;
-            }
-            else{
-                if(prevLeft!=NULL){
-                    prevLeft->next=left;
-                    if(res==NULL) res=left;
-                }
-                break;
-            }
+        ListNode*dummy=new ListNode(-100);
+        dummy->next=head;
+        ListNode*temp=dummy;
+        while(temp->next!=NULL && temp->next->next!=NULL){
+            ListNode*a=temp->next;
+            ListNode*b=a->next;
+            a->next=b->next;
+            b->next=a;
+            temp->next=b;
+            temp=a;
         }
-        return res;
+        return dummy->next;
     }
 };
