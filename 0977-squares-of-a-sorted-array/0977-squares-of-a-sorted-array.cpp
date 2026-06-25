53854b23dbe7602d9b3ec793c3913ffcf531da43
Karan Mishra <150538561+karanmishra-dev@users.noreply.github.com>
2026-06-25 06:57:55 +0530

    Time: 0 ms (100%), Space: 30.1 MB (87.12%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -7,7 +7,7 @@ public:
         int j=n-1;
         int k=n-1;
         while(i<=j){
-            if(nums[i]*nums[i]<nums[j]*nums[j]){
+            if(nums[i]*nums[i]<=nums[j]*nums[j]){
                 ans[k]=nums[j]*nums[j];
                 j--;
                 k--;
@@ -19,36 +19,5 @@ public:
             }
         }
         return ans;
-
-        //ONE WAY IS TO SPLIT ARRAY IN NEG AND POS ARRAY
-        // vector<int>neg;
-        // vector<int>pos;
-        // for(int num:nums){
-        //     if(num<0) neg.push_back(num);
-        //     else pos.push_back(num);
-        // }
-        // if(neg.size()==0){
-        //     for(int i=0;i<pos.size();i++)pos[i]=pos[i]*pos[i];
-        //     return pos;
-        // }
-        // if(pos.size()==0){
-        //     for(int i=0;i<neg.size();i++) neg[i]=neg[i]*neg[i];
-        //     reverse(neg.begin(),neg.end());
-        //     return neg;
-        // }
-        // for(int i=0;i<neg.size();i++) neg[i]=neg[i]*neg[i];
-        // reverse(neg.begin(),neg.end());
-        // for(int i=0;i<pos.size();i++) pos[i]=pos[i]*pos[i];
-        // int i=0;
-        // int j=0;
-        // vector<int>res(neg.size()+pos.size());
-        // int k=0;
-        // while(i<neg.size() && j<pos.size()){
-        //     if(neg[i]<=pos[j]) res[k++]=neg[i++];
-        //     else res[k++]=pos[j++];
-        // }
-        // while(i<neg.size()) res[k++]=neg[i++];
-        // while(j<pos.size()) res[k++]=pos[j++];
-        // return res;
     }
 };
