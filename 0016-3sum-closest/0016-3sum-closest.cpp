27be6948acfec5571b7249c98cf81e4e3035c3b8
Karan Mishra <150538561+karanmishra-dev@users.noreply.github.com>
2026-06-27 08:49:18 +0530

    Time: 12 ms (43.54%), Space: 14.2 MB (6.97%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -3,22 +3,30 @@ public:
     int threeSumClosest(vector<int>& nums, int target) {
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        int real_sum=nums[0]+nums[1]+nums[2];
-        int diff=abs(real_sum-target);
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
+        int real_sum=0;
+        int dis=INT_MAX;
+        for(int i=0;i<n;i++){
             int j=i+1;
             int k=n-1;
             while(j<k){
                 int sum=nums[i]+nums[j]+nums[k];
                 int d=abs(sum-target);
-                if(d<diff){
-                    diff=d;
-                    real_sum=sum;
-                }
                 if(sum==target) return sum;
-                if(sum<target) j++;
-                else k--;
+                else if(sum<target){
+                    if(d<=dis){
+                        dis=d;
+                        real_sum=sum;
+                        
+                    }
+                    j++;
+                }
+                else{
+                    if(d<=dis){
+                        dis=d;
+                        real_sum=sum;
+                    }
+                    k--;
+                }
             }
         }
         return real_sum;
