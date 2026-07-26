76949d67bbe5e4a16aa7827e3be7224fb5a44196
Karan Mishra <150538561+karanmishra-dev@users.noreply.github.com>
2026-07-26 23:41:04 +0530

    Time: 0 ms (100%), Space: 14.3 MB (59.76%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& a, vector<int>& b) {
-        sort(a.begin(),a.end());
-        sort(b.begin(),b.end());
-        int i=0;
-        int j=0;
+        // sort(a.begin(),a.end());
+        // sort(b.begin(),b.end());
+        // int i=0;
+        // int j=0;
+        // vector<int>ans;
+        // while(i<a.size() && j<b.size()){
+        //     if(a[i]==b[j]){
+        //         ans.push_back(a[i]);
+        //         while(i+1<a.size() && a[i]==a[i+1]) i++;
+        //         while(j+1<b.size() && b[j]==b[j+1]) j++;
+        //         i++;
+        //         j++;
+        //     }
+        //     else{
+        //         if(a[i]<b[j]) i++;
+        //         else j++;
+        //     }
+        // }
+        // return ans;
+
+        set<int>st(a.begin(),a.end());
         vector<int>ans;
-        while(i<a.size() && j<b.size()){
-            if(a[i]==b[j]){
-                ans.push_back(a[i]);
-                while(i+1<a.size() && a[i]==a[i+1]) i++;
-                while(j+1<b.size() && b[j]==b[j+1]) j++;
-                i++;
-                j++;
-            }
-            else{
-                if(a[i]<b[j]) i++;
-                else j++;
+        for(int x:a) st.insert(x);
+        for(int x:b){
+            if(st.find(x)!=st.end()){
+                ans.push_back(x);
+                st.erase(x);
             }
         }
         return ans;
